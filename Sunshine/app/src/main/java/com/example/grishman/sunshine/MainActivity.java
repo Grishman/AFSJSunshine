5100f810e34194cbae0352d52773ef9c50e7939c
Andrew Grishman <grishman4life@gmail.com>
2015-02-15 22:08:48 +0200

    Add maplocation
    and more staff

--- Sunshine/app/src/main/java/com/example/grishman/sunshine/MainActivity.java
+++ Sunshine/app/src/main/java/com/example/grishman/sunshine/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.grishman.sunshine;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.AsyncTask;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
@@ -24,6 +27,8 @@ import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private static final String LOG_TAG ="LOLCA" ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,10 +62,34 @@ public class MainActivity extends ActionBarActivity {
             startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
+        if (id==R.id.action_preferedlocation){
+            Uri geoLoc = Uri.parse("geo:47.6,-122.3");
+            showMap();
+            return true;
+        }
 
         return super.onOptionsItemSelected(item);
     }
-
+    public void showMap() {
+        SharedPreferences sharedPrefs =
+                PreferenceManager.getDefaultSharedPreferences(this);
+        String location = sharedPrefs.getString(
+                getString(R.string.pref_location_key),
+                getString(R.string.pref_location_default));
+// Using the URI scheme for showing a location found on a map. This super-handy
+// intent can is detailed in the "Common Intents" page of Android's developer site:
+// http://developer.android.com/guide/components/intents-common.html#Maps
+        Uri geoLocation = Uri.parse("geo:0,0?").buildUpon()
+                .appendQueryParameter("q", location)
+                .build();
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(geoLocation);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d(LOG_TAG, "Couldn't call " + location + ", no receiving apps installed!");
+        }
+    }
 }
 
 
