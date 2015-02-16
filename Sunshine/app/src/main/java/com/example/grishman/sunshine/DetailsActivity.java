450e6bdabdc9c31d3c3e63b59f60185cb7a1095a
Andrew Grishman <grishman4life@gmail.com>
2015-02-16 23:27:02 +0200

    Add Share Button

--- Sunshine/app/src/main/java/com/example/grishman/sunshine/DetailsActivity.java
+++ Sunshine/app/src/main/java/com/example/grishman/sunshine/DetailsActivity.java
@@ -2,12 +2,16 @@ package com.example.grishman.sunshine;
 
 import android.content.Intent;
 import android.support.v4.app.NavUtils;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -63,7 +67,12 @@ public class DetailsActivity extends ActionBarActivity {
      */
     public static class PlaceholderFragment extends Fragment {
 
+        private static final Object FORECAST_SHARE_HASHTAG = "#GrishmanRules";
+        private static final String LOG_TAG = "Details Share";
+        private String mForecastStr;
+
         public PlaceholderFragment() {
+            setHasOptionsMenu(true);
         }
 
         @Override
@@ -71,9 +80,37 @@ public class DetailsActivity extends ActionBarActivity {
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_details, container, false);
             Intent mainIntent = getActivity().getIntent();
+            mForecastStr=mainIntent.getStringExtra("ForeCastData");
             TextView  detailsText = (TextView )rootView.findViewById(R.id.detailsText);
-            detailsText.setText(mainIntent.getStringExtra("ForeCastData"));
+            detailsText.setText(mForecastStr);
             return rootView;
+        }
+
+        @Override
+        public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+            inflater.inflate(R.menu.details, menu);
+
+            MenuItem menuItem = menu.findItem(R.id.menu_item_share);
+
+            // Get the provider and hold onto it to set/change the share intent.
+            ShareActionProvider mShareActionProvider =
+                    (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+            // Attach an intent to this ShareActionProvider. You can update this at any time,
+             // like when the user selects a new piece of data they might like to share.
+            if (mShareActionProvider != null ) {
+                mShareActionProvider.setShareIntent(createShareForecastIntent());
+            } else {
+                Log.d(LOG_TAG, "Share Action Provider is null?");
+            }
+        }
+
+        private Intent createShareForecastIntent() {
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(Intent.EXTRA_TEXT,
+                    mForecastStr + FORECAST_SHARE_HASHTAG);
+            return shareIntent;
         }
     }
 }
