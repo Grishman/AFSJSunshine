bfe104af5cd06db48012b4e63b2cdc9226810faf
Andrew Grishman <grishman4life@gmail.com>
2015-03-01 18:09:01 +0200

    UriTest

--- Sunshine/app/src/main/java/com/example/grishman/sunshine/data/WeatherProvider.java
+++ Sunshine/app/src/main/java/com/example/grishman/sunshine/data/WeatherProvider.java
@@ -110,14 +110,17 @@ public class WeatherProvider extends ContentProvider {
     static UriMatcher buildUriMatcher() {
         // 1) The code passed into the constructor represents the code to return for the root
         // URI.  It's common to use NO_MATCH as the code for this case. Add the constructor below.
-
+         final UriMatcher mURIMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+        String authority = WeatherContract.CONTENT_AUTHORITY;
 
         // 2) Use the addURI function to match each of the types.  Use the constants from
         // WeatherContract to help define the types to the UriMatcher.
-
-
+        mURIMatcher.addURI(authority,WeatherContract.PATH_WEATHER,WEATHER);
+        mURIMatcher.addURI(authority,WeatherContract.PATH_WEATHER+"/*",WEATHER_WITH_LOCATION);
+        mURIMatcher.addURI(authority,WeatherContract.PATH_WEATHER+"/*/#",WEATHER_WITH_LOCATION_AND_DATE);
+        mURIMatcher.addURI(authority,WeatherContract.PATH_LOCATION,LOCATION);
         // 3) Return the new matcher!
-        return null;
+        return mURIMatcher;
     }
 
     /*
